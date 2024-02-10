c64bdf21149dd4ee54578dfc95cb00cad9925ef0
Julen Pardo <julen.pardo@outlook.es>
2024-02-10 17:52:09 +0100

    WIP SDL

--- src/gameboy_main.cpp
+++ src/gameboy_main.cpp
@@ -1,11 +1,49 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 #include <string>
+#include <SDL2/SDL_thread.h>
+
 #include "gameboy.hpp"
 
 
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 140
+
+int threadFunction(void* data) {
+    std::cout << "Hello from thread";
+
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     //std::string romPath = argv[1];
-    std::string romPath = "roms/cpu_instrs.gb";
+    std::string romPath = "roms/tetris.gb";
     GameBoy gameBoy = GameBoy(romPath);
+
+    /*
+    SDL_Window* window = NULL;
+    SDL_Surface* surface = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Event e;
+    bool quit = false; 
+
+    while (quit == false) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    */
+
     gameBoy.mainLoop();
 }
--- src/gpu/gpu.cpp
+++ src/gpu/gpu.cpp
@@ -1,8 +1,32 @@
 #include <stdint.h>
 #include "gpu.hpp"
 #include "lcd_control.hpp"
+#include "tile.hpp"
+
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 140
 
 
-void update(uint8_t cycles) {
+void GPU::init_sdl() {
+    SDL_Init(SDL_INIT_VIDEO);
 
+
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    surface = SDL_GetWindowSurface(window);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    //SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+    //SDL_UpdateWindowSurface(window);
+}
+
+void GPU::update(uint8_t cycles) {
+}
+
+
+TileVector getTiles() {
+    
 }
